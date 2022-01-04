66ce21e434cad8af520c1445283046393228d4b2
Ibrahim Ali <59145840+ByIbrahimAli@users.noreply.github.com>
2022-01-04 18:26:51 +0000

    moving onto primitive types. identifying int min and max values

---
+++ src/java_masterclass/primitiveTypes_ByteShortIntLong/Main.java
@@ -0,0 +1,13 @@
+//todo why isn't package working? It won't auto complete
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 100000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Min: " + myMinIntValue + "\nInteger Max: " + myMaxIntValue);
+    }
+}
