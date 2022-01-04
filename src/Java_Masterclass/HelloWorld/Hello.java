b72b22bf39bb72f2329532c94be4f10229178a0b
Ibrahim Ali <59145840+ByIbrahimAli@users.noreply.github.com>
2022-01-04 18:08:13 +0000

    int = int * 2

--- src/Java_Masterclass/HelloWorld/Hello.java
+++ src/Java_Masterclass/HelloWorld/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
         int myFirstNumber = (5 + 10) + (2 * 10);
         int mySecondNumber = 12;
-        int myThirdNumber = 6;
+        int myThirdNumber = myFirstNumber * 2;
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
         System.out.println(myTotal);
 
