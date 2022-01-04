b35ce053a5a2e8a0f02a6986ecc092833f415ffd
Ibrahim Ali <59145840+ByIbrahimAli@users.noreply.github.com>
2022-01-04 18:03:09 +0000

    BIDMAS expression in one int

--- src/Java_Masterclass/HelloWorld/Hello.java
+++ src/Java_Masterclass/HelloWorld/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main (String[] args){
         System.out.println("Hello sexy");
 
-        int myFirstNumber = 5 + 10;
+        int myFirstNumber = (5 + 10) + (2 * 10);
         System.out.println(myFirstNumber);
     }
 }
