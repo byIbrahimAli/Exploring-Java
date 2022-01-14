20ad95321d66ed7ed1f9ede3e026cc1478ff5a7e
Ibrahim Ali <59145840+ByIbrahimAli@users.noreply.github.com>
2022-01-14 11:14:08 +0000

    method to filter only letters and spaces

--- src/random_pile_of_sh/letters_only/Letters_Only.java
+++ src/random_pile_of_sh/letters_only/Letters_Only.java
@@ -10,8 +10,15 @@ public class Letters_Only {
         this.input = input;
     }
 
+    public String letter_filter() {
+        String input_copy = input;
+        input_copy = input_copy.replaceAll("[^a-zA-Z\s]", "");
+        // want no spaces too? Use: [^a-zA-Z]
+        return input_copy;
+    }
+
     public static void main(String[] args) {
-        Letters_Only test = new Letters_Only("Fuck");
-        System.out.println(test.input);
+        Letters_Only test = new Letters_Only("1M34y n3a89*m3e i1__s F4^uc12k__ ");
+        System.out.println(test.letter_filter());
     }
 }
