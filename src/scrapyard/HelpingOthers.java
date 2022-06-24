f47027a146b18b39f8763ef0e3c85fdbd0cb7df5
Ibrahim <59145840+byIbrahimAli@users.noreply.github.com>
2022-06-24 17:52:17 +0100

    count now counts total marks

--- src/scrapyard/HelpingOthers.java
+++ src/scrapyard/HelpingOthers.java
@@ -5,19 +5,23 @@ import java.util.Scanner;
 public class HelpingOthers {
     public static void main(String[] args) {
         int count = 0;
-        Scanner input = new Scanner(System.in);
         String students[] = new String[4];
         int marks[] = new int[4];
+        
+        Scanner input = new Scanner(System.in);
+
         for(String stud: students ){
             System.out.println("Enter Name:");
             stud = input.nextLine();
         }
+        
         for(int i = 0 ; i < marks.length; i++){
            System.out.println("enter Grade:");
            marks[i] = input.nextInt();
+           count = count + marks[i];
            if(i == (marks.length - 1)){
-             count++;
              System.out.println("Total is " + count);
+             input.close();
             }
         }   
     }
